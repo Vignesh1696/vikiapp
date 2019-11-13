9fc5eee9b13767392d35c470c130774e21e0d390
Vicky <54030715+Vignesh1696@users.noreply.github.com>
2019-11-13 17:11:32 +0530

    changes

--- index.js
+++ index.js
@@ -24,5 +24,4 @@ io.sockets.on('connection', function(socket) {
 });
 
 const server = http.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
